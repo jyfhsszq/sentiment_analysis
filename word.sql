df50f42f5c27db6772b9a97a271ec7e2a33b90bc
pjing <pjing@coupang.com>
2018-10-21 14:58:51 +0800

    add product level, attribute level, and service level key words

--- word.sql
+++ word.sql
@@ -1,5 +1,21 @@
-SELECT count(*) FROM reviews.words;
+SELECT count(*) FROM reviews.words where name = 'logistics';
 
 SELECT name, count(*) as cnt FROM reviews.words 
-where name not in ('i','%')
+where name not in ('i','%','book','movie','time','cd','album', 'one','lot','nothing','something','anyone','lot')
 group by name order by cnt desc
+
+PN: product name
+
+
+product level: book movie time cd album product file film music game song video sound video gift
+               novel 
+attribute level: story, way, quality, money, life, thing, price, author, version, prlblem
+band item voice picture size person player color brand level age volumn seller photo audio
+
+service level: service time logistics delivery suuport respnse
+
+
+experience level: 
+effect/result/impact: fun love waste taster help horror trouble mood experience interesting
+
+other: 
