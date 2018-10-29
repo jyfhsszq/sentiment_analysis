1db59fc13ad0623be8390c0010bf16c63f9cbd08
pjing <pjing@coupang.com>
2018-10-29 22:35:44 +0800

    test nsubj parser

--- nsubj_parser.py
+++ nsubj_parser.py
@@ -19,22 +19,45 @@ class NsubjParser(object):
         children = v_node[Tree.CHILDREN_KEY]
         for child in children:
             id = child[Tree.ID]
-            if(self.is_verb(id)):
+            if self.is_verb(id):
                 self.parse_verb(id, result)
 
-            if(self.is_adv(id)):
+            if self.is_adv(id):
                 adv_list = [id]
-                self.parse_adv(id, adv_list)
+                self.parse_adj_adv(id, adv_list)
                 result.append(SentimentUnit(verb, '', adv_list))
 
-    def parse_adv(self, adv, result=[]):
-        adv_node = self.tree.nodes[adv]
-        if Tree.CHILDREN_KEY in adv_node:
-            children = adv_node[Tree.CHILDREN_KEY]
-            for child in children:
-                id = child[Tree.ID]
-                if self.is_adv(id):
-                    self.parse_adv(id, result)
+            if self.is_noun(id):
+                self.parse_n(id, result)
+
+    def parse_adj_adv(self, ad, result=[]):
+        ad_node = self.tree.nodes[ad]
+        if Tree.CHILDREN_KEY not in ad_node:
+            return
+        children = ad_node[Tree.CHILDREN_KEY]
+        for child in children:
+            id = child[Tree.ID]
+            if self.is_adv(id) or self.is_adj(id):
+                self.parse_adj_adv(id, result)
+
+    def parse_n(self, n, result=[]):
+        n_node = self.tree.nodes[n]
+        if Tree.CHILDREN_KEY not in n_node:
+            return
+        children = n_node[Tree.CHILDREN_KEY]
+        for child in children:
+            id = child[Tree.ID]
+            if self.is_noun(id):
+                self.parse_n(id, result)
+
+            adj = -1
+            adv_list = []
+            if self.is_adj(id):
+                adj = id
+                self.parse_adj_adv(id, adv_list)
+
+            if adj > 0 or adv_list.__len__() > 0:
+               result.append(SentimentUnit(n, adj, adv_list))
 
     def find_words_list(self, number_list):
         return [self.words[id] for id in number_list]
@@ -46,3 +69,11 @@ class NsubjParser(object):
     def is_adv(self, node_id):
         tag = self.tags[node_id-1][1]
         return tag.startswith('RB')
+
+    def is_noun(self, node_id):
+        tag = self.tags[node_id-1][1]
+        return tag.startswith('N')
+
+    def is_adj(self, node_id):
+        tag = self.tags[node_id-1][1]
+        return tag.startswith('JJ')
